5f20f8486bbe50fc1e91533c3cfe71544824bdfd
Angel Tsvetkov <mrproper86@gmail.com>
2019-11-29 10:20:33 +0200

    update

---
+++ _includes/sections/customers/script.vue
@@ -0,0 +1,30 @@
+new Vue({
+    el: "#{{ include.id }}",
+    data: {
+        current: {{site.data.site.customers | jsonify }},
+        backup: {{site.data.site.customers | jsonify }}
+    },
+    mounted() {
+        try {
+            $("#{{include.id}}-activate").animatedModal({
+                "color": "#ffffff",
+                "animatedIn": "slideInDown",
+                "animatedOut": "slideOutDown"
+            });
+        }
+        catch(err) {
+
+        }
+        axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
+    },
+    methods: {
+        close: function () {
+            Object.assign(this.current, this.backup);
+        },
+        save: function () {
+           
+        }
+    }
+});
